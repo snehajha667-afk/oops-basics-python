fbfc4e8617f19790440bb303d7975f674c6307c2
snehajha667-afk <snehajha667@gmail.com>
2026-07-01 20:34:12 +0530

    polymorphism.py

---
+++ polymorphism.py
@@ -0,0 +1,13 @@
+class Dog:
+    def sound(self):
+        print("Dog says Woof!")
+
+class Cat:
+    def sound(self):
+        print("Cat says Meow!")
+
+dog = Dog()
+cat = Cat()
+
+dog.sound()
+cat.sound()
