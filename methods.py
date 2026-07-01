ed43b9f56966930b0d152fc5feb23106a11ee791
snehajha667-afk <snehajha667@gmail.com>
2026-07-01 20:27:39 +0530

    methods.py

---
+++ methods.py
@@ -0,0 +1,12 @@
+lass Calculator:
+
+    def add(self, a, b):
+        print("Addition =", a + b)
+
+    def subtract(self, a, b):
+        print("Subtraction =", a - b)
+
+calc = Calculator()
+
+calc.add(20, 10)
+calc.subtract(20, 10)
