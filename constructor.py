d720298ef7d3729109d1323382db5428ea8fa65f
snehajha667-afk <snehajha667@gmail.com>
2026-07-01 20:23:49 +0530

    constructor.py

---
+++ constructor.py
@@ -0,0 +1,13 @@
+class Student:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def display(self):
+        print("Name:", self.name)
+        print("Age:", self.age)
+
+student1 = Student("Sneha", 21)
+
+student1.display()
+
