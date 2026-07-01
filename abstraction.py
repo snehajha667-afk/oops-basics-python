f87b179121d37599ac1acc034317314880b2bdfc
snehajha667-afk <snehajha667@gmail.com>
2026-07-01 20:46:32 +0530

    abstraction.py

---
+++ abstraction.py
@@ -0,0 +1,16 @@
+
+from abc import ABC, abstractmethod
+
+class Animal(ABC):
+
+    @abstractmethod
+    def sound(self):
+        pass
+
+class Dog(Animal):
+
+    def sound(self):
+        print("Dog barks.")
+
+dog = Dog()
+dog.sound()
